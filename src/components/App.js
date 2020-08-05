d29f6e9a9b3c53ab6e47fdafb4983a0cf1b0156e
h-cue <hh222nx@student.lnu.se>
2020-08-05 23:54:05 +0200

    full structure refactor into modules

---
+++ src/components/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Player from './Player';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: 'Jerry',
+        score: 0,
+        id: 1 
+      },
+      {
+        name: 'Mary',
+        score: 0,
+        id: 2
+      },
+      {
+        name: 'Harry',
+        score: 0,
+        id: 3
+      },
+      {
+        name: 'Terry',
+        score: 0,
+        id: 4
+      },
+      {
+        name: 'Larry',
+        score: 0,
+        id: 5
+      }
+    ]
+  }
+
+  handleScoreChange = (playerIndex, change) => {
+    this.setState( prevState => {
+      const updatedPlayers = [ ...prevState.players ];
+      const updatedPlayer = { ...updatedPlayers[playerIndex] };
+      updatedPlayer.score += change;
+      updatedPlayers[playerIndex] = updatedPlayer;
+
+      // Update the player's state without mutating the original
+      return {
+        players: updatedPlayers
+      };
+    });
+  }
+
+  handleHemovePlayer = id => {
+    this.setState( prevState => {
+        return (
+          {players: prevState.players.filter( play => play.id !== id )}
+        )
+    });
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header 
+          title="Scoreboard" 
+          totalPlayers={this.state.players.length}   
+        />  
+  
+        {this.state.players.map( (player, index) => {  
+          return (
+            <Player 
+              score={player.score}
+              removePlayer={this.handleHemovePlayer}
+              id={player.id}
+              index={index}
+              name={player.name} 
+              changeScore={this.handleScoreChange}
+              key={player.id.toString()} 
+            />
+          )
+        })} 
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+export default App;
